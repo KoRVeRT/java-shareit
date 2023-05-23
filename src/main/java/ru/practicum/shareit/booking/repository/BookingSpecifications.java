f86be207b0b9f395f0f64a4fe7e898aed07ffc06
Masharov Dmitry <staker@yandex.ru>
2023-05-23 22:13:13 +0300

    refactor BookingSpecifications

--- src/main/java/ru/practicum/shareit/booking/repository/BookingSpecifications.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingSpecifications.java
@@ -14,14 +14,20 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BookingSpecifications {
     private static final String BOOKING_START_DATE_FIELD_NAME = "start";
+    private static final String BOOKING_END_DATE_FIELD_NAME = "end";
+    private static final String BOOKING_ID_FIELD_NAME = "id";
+    private static final String BOOKING_ITEM_FIELD_NAME = "item";
+    private static final String BOOKING_BOOKER_FIELD_NAME = "booker";
     private static final String BOOKING_STATUS_FIELD_NAME = "status";
 
     public static Specification<Booking> existsBookerIdAndItemIdAndEndBefore(
             Long bookerId, Long itemId, LocalDateTime time) {
         return (root, query, builder) -> {
-            Predicate bookerIdPredicate = builder.equal(root.get("booker").get("id"), bookerId);
-            Predicate itemIdPredicate = builder.equal(root.get("item").get("id"), itemId);
-            Predicate endPredicate = builder.lessThan(root.get("end"), time);
+            Predicate bookerIdPredicate = builder.equal(root.get(BOOKING_BOOKER_FIELD_NAME).get(BOOKING_ID_FIELD_NAME),
+                    bookerId);
+            Predicate itemIdPredicate = builder.equal(root.get(BOOKING_ITEM_FIELD_NAME).get(BOOKING_ID_FIELD_NAME),
+                    itemId);
+            Predicate endPredicate = builder.lessThan(root.get(BOOKING_END_DATE_FIELD_NAME), time);
             Predicate statusPredicate = builder.equal(root.get(BOOKING_STATUS_FIELD_NAME), BookingStatus.APPROVED);
             return builder.and(bookerIdPredicate, itemIdPredicate, endPredicate, statusPredicate);
         };
@@ -30,7 +36,8 @@ public class BookingSpecifications {
     public static Specification<Booking> findFirstByItemIdAndStartBeforeOrderByStartDesc(
             Long itemId, LocalDateTime time) {
         return (root, query, builder) -> {
-            Predicate itemIdPredicate = builder.equal(root.get("item").get("id"), itemId);
+            Predicate itemIdPredicate = builder.equal(root.get(BOOKING_ITEM_FIELD_NAME).get(BOOKING_ID_FIELD_NAME),
+                    itemId);
             Predicate startPredicate = builder.lessThan(root.get(BOOKING_START_DATE_FIELD_NAME), time);
             Predicate statusPredicate = builder.notEqual(root.get(BOOKING_STATUS_FIELD_NAME), BookingStatus.REJECTED);
             query.orderBy(builder.desc(root.get(BOOKING_START_DATE_FIELD_NAME)));
@@ -41,7 +48,8 @@ public class BookingSpecifications {
     public static Specification<Booking> findFirstByItemIdAndStartAfterOrderByStart(
             Long itemId, LocalDateTime time) {
         return (root, query, builder) -> {
-            Predicate itemIdPredicate = builder.equal(root.get("item").get("id"), itemId);
+            Predicate itemIdPredicate = builder.equal(root.get(BOOKING_ITEM_FIELD_NAME).get(BOOKING_ID_FIELD_NAME),
+                    itemId);
             Predicate startPredicate = builder.greaterThan(root.get(BOOKING_START_DATE_FIELD_NAME), time);
             Predicate statusPredicate = builder.equal(root.get(BOOKING_STATUS_FIELD_NAME), BookingStatus.APPROVED);
             query.orderBy(builder.asc(root.get(BOOKING_START_DATE_FIELD_NAME)));
