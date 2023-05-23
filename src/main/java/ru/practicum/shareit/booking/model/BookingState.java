f2bd5ba6c19330814a4a9e88f45cc79e8630e808
Masharov Dmitry <staker@yandex.ru>
2023-05-23 22:00:29 +0300

    refactor BookingState

--- src/main/java/ru/practicum/shareit/booking/model/BookingState.java
+++ src/main/java/ru/practicum/shareit/booking/model/BookingState.java
@@ -7,15 +7,15 @@ import java.time.LocalDateTime;
 public enum BookingState {
     ALL((r, q, cb) -> cb.isTrue(cb.literal(true))),
     CURRENT((r, q, cb) -> cb.between(
-            cb.literal(LocalDateTime.now()), r.<LocalDateTime>get("start"), r.<LocalDateTime>get("end"))),
-    PAST((r, q, cb) -> cb.lessThan(r.<LocalDateTime>get("end"), LocalDateTime.now())),
-    FUTURE((r, q, cb) -> cb.greaterThan(r.<LocalDateTime>get("start"), LocalDateTime.now())),
+            cb.literal(LocalDateTime.now()), r.get("start"), r.get("end"))),
+    PAST((r, q, cb) -> cb.lessThan(r.get("end"), LocalDateTime.now())),
+    FUTURE((r, q, cb) -> cb.greaterThan(r.get("start"), LocalDateTime.now())),
     WAITING((r, q, cb) -> cb.equal(r.<BookingStatus>get("status"), BookingStatus.WAITING)),
     REJECTED((r, q, cb) -> cb.equal(r.<BookingStatus>get("status"), BookingStatus.REJECTED));
 
     private final Specification<Booking> specification;
 
-    private BookingState(Specification<Booking> specification) {
+    BookingState(Specification<Booking> specification) {
         this.specification = specification;
     }
 
